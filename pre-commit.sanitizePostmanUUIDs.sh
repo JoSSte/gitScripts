42624a5874b0a1f2b7686da81939dbb825dd4fd0
JoSSte <JoSSte@users.noreply.github.com>
2020-11-12 16:43:38 +0100

    Added code to handle filenames with spaces

--- pre-commit.sanitizePostmanUUIDs.sh
+++ pre-commit.sanitizePostmanUUIDs.sh
@@ -5,11 +5,17 @@
 # This is done to ensure that the differences on your code is what actually gives you value, and not a load of UUIDs that change on every commit.
 # The file is staged and a message is displayed
 
+#save delimiter
+SAVEIFS=$IFS
+#set delimiter to something else than space
+IFS=$(echo -en "\n\b")
+
 #get the changed files
-files=`git diff --cached --name-status | awk '$1 != "D" { print $2 }'`
+files=`git diff --staged --name-only`
 #set changed flag to false
 CHANGED=false
 for filename in $files; do
+    printf "\narguments [$filename]\n\n"
     if [ "${filename: -24}" == ".postman_collection.json" ]; then
         sed -i -r 's/"(_postman_id|id)": "([a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})"/"\1": "00000000-0000-0000-0000-000000000000"/gm' "$filename"
         git add $filename
@@ -17,6 +23,11 @@ for filename in $files; do
         CHANGED=true
     fi
 done
+
+#reset delimiter
+IFS=$SAVEIFS
+
+
 # if files have been changed (potentially) display a message and abort the commit
 if $CHANGED; then
     echo "PRE-COMMIT: Postman collection found. UUIDs have been sanitized. Please verify and recommit"
